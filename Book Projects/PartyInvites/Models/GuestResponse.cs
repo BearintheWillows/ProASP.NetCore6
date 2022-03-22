174a913e94a19a115622afbab72a0e6381764210
BearintheWillows <sjwillows91@outlook.com>
2022-03-22 17:00:36 +0000

    Completed First Project and Party Invites

---
+++ Book Projects/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace PartyInvites.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class GuestResponse
+{
+    [Required( ErrorMessage = "Please enter your name" )]
+    public string? Name { get; set; }
+    [Required( ErrorMessage = "Please enter email address" )]
+    public string? Email { get; set; }
+    [Required( ErrorMessage = "Please enter phone number" )]
+    public string? PhoneNumber { get; set; }
+    [Required( ErrorMessage = "Please specify if you will attend" )]
+    public bool WillAttend { get; set; }
+}
---
+++ Book Projects/PartyInvites/Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿namespace PartyInvites.Models;
+
+public class Repository
+{
+    private static List<GuestResponse> _responses = new();
+
+    public static IEnumerable<GuestResponse> Responses => _responses;
+
+    public static void AddResponse( GuestResponse response )
+    {
+        Console.WriteLine( response );
+        _responses.Add( response );
+        Console.WriteLine( $"Current Responses: {_responses.Count}" );
+    }
+}
