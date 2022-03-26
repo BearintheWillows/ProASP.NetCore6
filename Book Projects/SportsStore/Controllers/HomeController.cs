ea2e31d5693abd80e79c1ad1b12e1ffc213a2712
BearintheWillows <sjwillows91@outlook.com>
2022-03-26 11:50:36 +0000

    Continued course

--- Book Projects/SportsStore/Controllers/HomeController.cs
+++ Book Projects/SportsStore/Controllers/HomeController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers;
 
 public class HomeController : Controller
 {
-	// GET
-	public IActionResult Index() => View();
+	private IStoreRepository _repository;
+	public  int              PageSize = 4;
+
+	public HomeController( IStoreRepository repo ) { _repository = repo; }
+
+	public ViewResult Index( int productPage = 1 ) =>
+		View(
+			_repository.Products
+			           .OrderBy( p => p.ProductID )
+			           .Skip( ( productPage - 1 ) * PageSize )
+			           .Take( PageSize ) );
 }
