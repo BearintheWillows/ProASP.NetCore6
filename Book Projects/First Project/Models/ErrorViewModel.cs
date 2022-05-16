efa548c91d6861494bf56a984dc5b0c944dfe755
BearintheWillows <sjwillows91@outlook.com>
2022-05-16 10:27:04 +0100

    Deleted previous attempt. Starting fresh.

--- Book Projects/First Project/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace First_Project.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty( RequestId );
-}
--- Book Projects/Testing/SimpleApp/Controllers/HomeController.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SimpleApp.Models;
-
-namespace SimpleApp.Controllers;
-public class HomeController : Controller
-{
-    public ViewResult Index() => View( Product.GetProducts() );
-}
