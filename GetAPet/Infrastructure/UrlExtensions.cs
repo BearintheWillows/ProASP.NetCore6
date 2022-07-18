41c19728e942b1692348ff2c27d3c2114bc6c74c
BearintheWillows <sjwillows91@outlook.com>
2022-07-18 08:47:26 +0100

    Added Donation page without HTML via Razor pages. Added Donate button to Pet list.

---
+++ GetAPet/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,9 @@
+namespace GetAPet.Infrastructure;
+
+public static class UrlExtensions
+{
+	public static string PathAndQuery(this HttpRequest request) =>
+	request.QueryString.HasValue
+		? $"{request.Path}{request.QueryString}"
+		: request.Path.ToString();
+}
