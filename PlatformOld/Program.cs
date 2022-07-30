d11ecf1103bb2f8c08fa19e5249c19ada402328c
BearintheWillows <sjwillows91@outlook.com>
2022-07-30 20:24:56 +0100

    Refactored to utilise Dependency injection

--- PlatformOld/Program.cs
+++ PlatformOld/Program.cs
@@ -4,20 +4,20 @@ using Platform.Services;
 using Platform.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
+
 var app = builder.Build();
 
 app.UseMiddleware<WeatherMiddleware>();
-	
-IResponseFormatter formatter = new TextResponseFormatter();
 
-
-app.MapGet("middleware/function", async (context) => {
-	await TypeBroker.Formatter.Format(context, "Middleware Function: It is snowing in Chicago");
+app.MapGet("middleware/function", async (HttpContext context, IResponseFormatter formatter) => {
+	await formatter.Format(context, "Middleware Function: It is snowing in Chicago");
 });
 
 app.MapGet("endpoint/class", WeatherEndpoint.Endpoint);
-app.MapGet("endpoint/function", async context => {
-	await context.Response.WriteAsync("Endpoint Function: It is sunny in LA");
+app.MapGet("endpoint/function", async (HttpContext context, IResponseFormatter formatter) => {
+	await formatter.Format(context, "Endpoint Function: It is sunny in LA");
 });
 
 app.Run();
