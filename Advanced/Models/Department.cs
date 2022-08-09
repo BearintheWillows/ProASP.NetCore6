2c00589e9c933238c608003f09845c0114f0427f
BearintheWillows <sjwillows91@outlook.com>
2022-08-09 21:36:13 +0100

    Part 4. Chapter 32 Setup complete.

---
+++ Advanced/Models/Department.cs
@@ -0,0 +1,7 @@
+namespace Advanced.Models;
+
+public class Department {
+	public long                 Departmentid { get; set; }
+	public string               Name         { get; set; } = String.Empty;
+	public IEnumerable<Person>? People       { get; set; }
+}
