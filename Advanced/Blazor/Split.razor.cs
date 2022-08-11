7ac4d6f4d33500e31df20d7193f217008bba041e
BearintheWillows <sjwillows91@outlook.com>
2022-08-11 07:34:49 +0100

    Completed Chapter 33

---
+++ Advanced/Blazor/Split.razor.cs
@@ -0,0 +1,12 @@
+using Advanced.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace Advanced.Blazor;
+
+public partial class Split {
+	[Inject]
+	public DataContext? Context { get; set; }
+	public IEnumerable<string> Names =>
+		Context?.People.Select(p => p.Firstname)
+	 ?? Enumerable.Empty<string>();
+}
---
+++ Advanced/Pages/Blazor.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Advanced.Pages;
+
+public class Blazor : PageModel
+{
+	public void OnGet()
+	{
+		
+	}
+}
